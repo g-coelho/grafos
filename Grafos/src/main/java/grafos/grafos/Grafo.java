a431bfc3d465a52d3e6b3f60198f7bb3943f8d91
g-coelho <gabrielcoelhodesouza@outlook.com>
2022-02-23 16:19:09 -0300

    Iniciada a correção do código, antes era usado um map de arestas, o que é um erro visto que é possível a existência de arestas iguais(multigrafos) e isso não seria registrado pelo map, já que ele não aceita valores repetidos e simplesmente substitui os iguais. Ainda é justificado o map de vértices visto que não teremos vértices repetidos.

--- Grafos/src/main/java/grafos/grafos/Grafo.java
+++ Grafos/src/main/java/grafos/grafos/Grafo.java
@@ -18,7 +18,6 @@ public class Grafo {
     private ArrayList<Vertice> vertices;
     private HashMap<String, Vertice> verticesMap;
     private ArrayList<Aresta> arestas;
-    private HashMap<String, Aresta> arestasMap;
     private boolean orientado;
     private boolean valorado;
     
@@ -27,7 +26,6 @@ public class Grafo {
         this.vertices = new ArrayList<>();
         this.arestas = new ArrayList<>();
         this.verticesMap = new HashMap<>();
-        this.arestasMap = new HashMap<>();
         this.orientado = orientado;
         this.valorado = valorado;
     }
@@ -39,29 +37,35 @@ public class Grafo {
     }
     
     public void removerVertice(String nome){         
-        arestasMap.entrySet().removeIf(chave -> chave.getValue().getOrigem().getNome().equals(nome));
-        arestasMap.entrySet().removeIf(chave -> chave.getValue().getDestino().getNome().equals(nome));
+        Iterator<Aresta> itrA = arestas.iterator();        
+        while (itrA.hasNext()){
+            Aresta a = itrA.next();            
+            if(a.getOrigem().getNome().equals(nome)| a.getDestino().getNome().equals(nome)){               
+                itrA.remove();
+            }               
+        }
+        
         vertices.remove(verticesMap.get(nome));
         verticesMap.remove(nome);   
     }
     
-    public void adicionarAresta(String origem, String destino, int valor){
-        Aresta a = new Aresta(verticesMap.get(origem), verticesMap.get(destino));        
-        arestas.add(a);     
-        arestasMap.put(verticesMap.get(origem).getNome() + verticesMap.get(destino).getNome(), a);
-        alterarValorAresta(origem, destino, valor);
-    }
-    
-    public void removerAresta(String origem, String destino){
-        arestasMap.remove(verticesMap.get(origem).getNome() + verticesMap.get(destino).getNome());
-        arestas.remove(arestasMap.get(verticesMap.get(origem).getNome() + verticesMap.get(destino).getNome()));
-    }
-    
-    public void alterarValorAresta(String origem, String destino, int valor){
-        arestasMap.get(origem + destino).setValor(valor);
-        arestas.remove(arestasMap.get(origem + destino));
-        arestas.add(arestasMap.get(origem + destino));
-    }
+//    public void adicionarAresta(String origem, String destino, int valor){
+//        Aresta a = new Aresta(verticesMap.get(origem), verticesMap.get(destino));        
+//        arestas.add(a);     
+//        arestasMap.put(verticesMap.get(origem).getNome() + verticesMap.get(destino).getNome(), a);
+//        alterarValorAresta(origem, destino, valor);
+//    }
+//    
+//    public void removerAresta(String origem, String destino){
+//        arestasMap.remove(verticesMap.get(origem).getNome() + verticesMap.get(destino).getNome());
+//        arestas.remove(arestasMap.get(verticesMap.get(origem).getNome() + verticesMap.get(destino).getNome()));
+//    }
+//    
+//    public void alterarValorAresta(String origem, String destino, int valor){
+//        arestasMap.get(origem + destino).setValor(valor);
+//        arestas.remove(arestasMap.get(origem + destino));
+//        arestas.add(arestasMap.get(origem + destino));
+//    }
     
     public int [][] gerarMatrixAdjacencia(){        
         int [][] matrAdj;
